820d6d1f0525c20090db94ac81d83ed70d91c92d
kei-g <km.8k6ce+github@gmail.com>
2021-08-23 21:31:27 +0900

    :hammer: Prepend shebang
    Signed-off-by: kei-g <km.8k6ce+github@gmail.com>

--- post-build.sh
+++ post-build.sh
@@ -1,27 +1,10 @@
 #!/bin/sh
 
 [ -d bin ] || mkdir bin || exit 1
-
-cd build && find bin -type f | while read name; do
-  echo '#!/usr/bin/env node' > ../$name || exit 1
-  npx terser $name --compress --mangle >> ../$name || exit 1
-  chmod +x ../$name || exit 1
-done
-
-cd ..
-
 [ -d lib ] || mkdir lib || exit 1
 
-cd build && find lib -type f | while read name; do
-  npx terser $name --compress --mangle > ../$name || exit 1
-done
-
-cd ..
-
-cd build && find . -maxdepth 1 -type f | while read name; do
-  echo '#!/usr/bin/env node' > ../$name || exit 1
-  npx terser $name --compress --mangle >> ../$name || exit 1
-  chmod +x ../$name || exit 1
+cd build && find . -type f | while read name; do
+  npx terser $name -c -m -o ../$name --toplevel || exit 1
 done
 
 cd ..
